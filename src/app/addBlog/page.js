99c561423b5c1e27fb49ade091234ed13ca9d43e
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2023-08-21 12:02:31 -0700

    created blog post form

--- src/app/addBlog/page.js
+++ src/app/addBlog/page.js
@@ -1,8 +1,14 @@
+'use client'
 import React from 'react'
+import BlogForm from '../../components/BlogForm'
 
 function page() {
   return (
-    <div><h1>Add Blog Post</h1></div>
+    <div className="flex flex-col gap-10">
+    <h1 className="text-2xl font-bold text-gray-800">Add a Blog Post</h1>
+    <hr />
+    <BlogForm />
+  </div>
   )
 }
 
